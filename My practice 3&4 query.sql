2c5e643bac3e629093fd69b2917b4f087039474b
Ramakrishnan E <110685034+Ramkirks@users.noreply.github.com>
2022-11-29 21:48:57 +0530

    Add files via upload

---
+++ My practice 3&4 query.sql
@@ -0,0 +1,92 @@
+
+Write a SQL query to display only the details of employees who either earn the highest salary
+or the lowest salary in each department from the employee table.
+
+--Tables Structure:
+
+drop table employee;
+create table employee
+( emp_ID int primary key
+, emp_NAME varchar(50) not null
+, DEPT_NAME varchar(50)
+, SALARY int);
+
+insert into employee values(101, 'Mohan', 'Admin', 4000);
+insert into employee values(102, 'Rajkumar', 'HR', 3000);
+insert into employee values(103, 'Akbar', 'IT', 4000);
+insert into employee values(104, 'Dorvin', 'Finance', 6500);
+insert into employee values(105, 'Rohit', 'HR', 3000);
+insert into employee values(106, 'Rajesh',  'Finance', 5000);
+insert into employee values(107, 'Preet', 'HR', 7000);
+insert into employee values(108, 'Maryam', 'Admin', 4000);
+insert into employee values(109, 'Sanjay', 'IT', 6500);
+insert into employee values(110, 'Vasudha', 'IT', 7000);
+insert into employee values(111, 'Melinda', 'IT', 8000);
+insert into employee values(112, 'Komal', 'IT', 10000);
+insert into employee values(113, 'Gautham', 'Admin', 2000);
+insert into employee values(114, 'Manisha', 'HR', 3000);
+insert into employee values(115, 'Chandni', 'IT', 4500);
+insert into employee values(116, 'Satya', 'Finance', 6500);
+insert into employee values(117, 'Adarsh', 'HR', 3500);
+insert into employee values(118, 'Tejaswi', 'Finance', 5500);
+insert into employee values(119, 'Cory', 'HR', 8000);
+insert into employee values(120, 'Monica', 'Admin', 5000);
+insert into employee values(121, 'Rosalin', 'IT', 6000);
+insert into employee values(122, 'Ibrahim', 'IT', 8000);
+insert into employee values(123, 'Vikram', 'IT', 8000);
+insert into employee values(124, 'Dheeraj', 'IT', 11000);
+
+select * from employee;
+
+
+select emp_id, emp_name, dept_name, salary
+from 
+   (select *, 
+    rank() over(PARTITION by dept_name ORDER by salary DESC)as rn
+    from employee)x
+where (x.rn) in (4,10,1)
+
+
+--From the doctors table, fetch the details of doctors who work in the same hospital but in different speciality.
+
+--Table Structure:
+
+drop table doctors;
+create table doctors
+(
+id int primary key,
+name varchar(50) not null,
+speciality varchar(100),
+hospital varchar(50),
+city varchar(50),
+consultation_fee int
+);
+
+insert into doctors values
+(1, 'Dr. Shashank', 'Ayurveda', 'Apollo Hospital', 'Bangalore', 2500),
+(2, 'Dr. Abdul', 'Homeopathy', 'Fortis Hospital', 'Bangalore', 2000),
+(3, 'Dr. Shwetha', 'Homeopathy', 'KMC Hospital', 'Manipal', 1000),
+(4, 'Dr. Murphy', 'Dermatology', 'KMC Hospital', 'Manipal', 1500),
+(5, 'Dr. Farhana', 'Physician', 'Gleneagles Hospital', 'Bangalore', 1700),
+(6, 'Dr. Maryam', 'Physician', 'Gleneagles Hospital', 'Bangalore', 1500);
+
+select * from doctors;
+
+select d1.name, d1.speciality,d1.hospital
+from doctors d1
+join doctors d2
+on d1.hospital = d2.hospital and d1.speciality <> d2.speciality
+and d1.id <> d2.id;
+
+
+select d1.name, d1.speciality,d1.hospital
+from doctors d1
+join doctors d2
+on d1.hospital = d2.hospital and d1.id <> d2.id;
+
+select e.*,
+lag(salary) over(partition by dept_name order by emp_id) as prev_empl_sal,
+case when e.salary > lag(salary) over(partition by dept_name order by emp_id) then 'Higher than previous employee'
+     when e.salary < lag(salary) over(partition by dept_name order by emp_id) then 'Lower than previous employee'
+	 when e.salary = lag(salary) over(partition by dept_name order by emp_id) then 'Same than previous employee' end as sal_range
+from employee e;
